6ff82bf5039bf607ab0ac1fad90faf1a210662bd
mwita-lnx <mwitalnx@gmail.com>
2025-03-30 13:54:13 +0300

    errors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,20 +2,11 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import checker from 'vite-plugin-checker';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),checker({
-    typescript: {
-      enabled: false, // Only enable in development
-      buildMode: true, // Ensure it runs during build in development
-    },
-    eslint: {
-      enabled: false, // Disable eslint if you want
-      buildMode: true, 
-    },
-  })],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
